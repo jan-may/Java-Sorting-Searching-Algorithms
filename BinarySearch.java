7d93b9651701a6d695728e0b7ddd9d56410df102
jan-may <78799602+jan-may@users.noreply.github.com>
2021-09-28 22:21:28 +0200

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,38 @@
+class BinarySearch
+{
+    public static int binarySearch(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            }
+
+            if (target < arr[mid]) {
+                right = mid-1;
+            }
+            if (target > arr[mid]) {
+                left = mid+1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 2, 5, 6, 8, 9, 10 };
+        int target = 6;
+
+        int index = binarySearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
