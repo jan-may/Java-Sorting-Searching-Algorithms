17a45337e7b789b01b9207de73e354f63908cea8
jan-may <78799602+jan-may@users.noreply.github.com>
2021-10-01 21:33:48 +0200

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,25 +1,18 @@
 class BinarySearch
 {
-    public static int binarySearch(int[] arr, int target)
-    {
+    public static int search(int[] arr, int target) {
         int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right)
-        {
-            int mid = (left + right) / 2;
-            if (target == arr[mid]) {
-                return mid;
-            }
-
-            if (target < arr[mid]) {
-                right = mid-1;
-            }
-            if (target > arr[mid]) {
-                left = mid+1;
+        int right = arr.length;
+        while (left < right) {
+            int mid = Math.floorDiv(left + right, 2);
+            int midValue = arr[mid];
+            if (midValue == target) return mid;
+            else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-
         return -1;
     }
 
@@ -28,7 +21,7 @@ class BinarySearch
         int[] arr = { 2, 5, 6, 8, 9, 10 };
         int target = 6;
 
-        int index = binarySearch(arr, target);
+        int index = search(arr, target);
         if (index != -1) {
             System.out.println("Element found at index " + index);
         } else {
